ff289f5d77f354a220ea1c620fb8432862576e13
Milena Julião <milenadjuliao@gmail.com>
2023-06-15 21:08:49 -0300

    Projeto base completo

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
                 sans: ['inter', 'sans-serif']
             },
             backgroundImage: {
-                galaxy: "url('./public/assets/background-galaxy.png')",
+                galaxy: "url('./assets/background-galaxy.png')",
                 'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 50.94%, #E1D55D 50.57%)',
                 'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)',
             }
